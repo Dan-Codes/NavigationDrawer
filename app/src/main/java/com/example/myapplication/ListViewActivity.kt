74bae2113520d1126a6589763c9c1a47a4a9b74f
dan-codes <dli123@syr.edu>
2019-11-05 20:28:49 -0500

    logo done

--- app/src/main/java/com/example/myapplication/ListViewActivity.kt
+++ app/src/main/java/com/example/myapplication/ListViewActivity.kt
@@ -1,11 +1,15 @@
 package com.example.myapplication
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.AttributeSet
 import android.util.Log
+import android.view.Menu
 import android.view.View
+import androidx.appcompat.view.menu.MenuPopupHelper
+import androidx.appcompat.widget.PopupMenu
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.get
 import com.example.myapplication.movieList
@@ -13,6 +17,8 @@ import com.example.myapplication.ui.gallery.GalleryFragment
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_list_view.*
 import kotlinx.android.synthetic.main.fragment_list_view.*
+import kotlinx.android.synthetic.main.fragment_movie.view.*
+
 var movieList: List<MovieData> = Gson().fromJson(movies, Array<MovieData>::class.java).asList()
 class ListViewActivity : AppCompatActivity(), GalleryFragment.OnListViewInteractionListener {
 
@@ -28,6 +34,11 @@ class ListViewActivity : AppCompatActivity(), GalleryFragment.OnListViewInteract
         supportFragmentManager.beginTransaction().replace(view.id, fragment).commit()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.movie_list_toolbar, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
     override fun onListViewInteraction(movie: MovieData, position: Int, view: View) {
         Log.d("test", "pressed")
         val title = movie.title
@@ -35,7 +46,8 @@ class ListViewActivity : AppCompatActivity(), GalleryFragment.OnListViewInteract
         val fragment = MovieFragment.newInstance(movie, posterid!!)
         val view = movieList
         supportFragmentManager.beginTransaction().replace(view.id, fragment).addToBackStack(null).commit()
-
+        var image: Drawable = getResources().getDrawable(posterid)
+        toolbar2.logo= image
     }
 
 
